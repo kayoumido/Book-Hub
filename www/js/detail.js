c0d510210bdbb015f9315a8ab72144fbb142e28b
KAYOUMI Doran <Doran.KAYOUMI@cpnv.ch>
2017-06-16 09:14:59 +0200

    Book information is retrieved from db

--- www/js/detail.js
+++ www/js/detail.js
@@ -1,2 +1,24 @@
 app.onPageInit('detail', function(page) {
+
+  // $$('.back').click(function() {
+  //   mainView.router.loadPage('index.html');
+  // });
+
+  var db = new DBHandler();
+  var dbh = db.getDBH();
+
+  dbh.transaction(function(tx) {
+    tx.executeSql(`select * from books where id = ${page.query.id}`, [], function(tx, data) {
+      // get template from page
+      var template = $$('#book-detail-template').html();
+      // compile it with Template7
+      var compiledTemplate = Template7.compile(template);
+      // insert data into template
+      var html = compiledTemplate(data.rows);
+      // add it to page
+      $$('.book-detail-container').html(html);
+
+    }, null);
+  });
+
 });
