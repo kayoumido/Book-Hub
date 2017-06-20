f22127af2645af341b845d9166c2b5b6aec00b93
KAYOUMI Doran <doran.kayoumi@cpnv.ch>
2017-06-20 08:27:35 +0200

    Added click event on eye and star. When clicked, a class is added (to change colour) and value in db is updated

--- www/js/detail.js
+++ www/js/detail.js
@@ -1,6 +1,6 @@
 app.onPageInit('detail', function(page) {
 
-  var db = new DBHandler();
+  var db  = new DBHandler();
   var dbh = db.getDBH();
 
   dbh.transaction(function(tx) {
@@ -18,13 +18,40 @@ app.onPageInit('detail', function(page) {
   });
 
   $$('.delete-btn').click(function() {
-    var db = new DBHandler();
+    var db  = new DBHandler();
     var dbh = db.getDBH();
 
     dbh.transaction(function(tx) {
       tx.executeSql('delete from books where id = ?', [$$('.book-detail').data('id')], null, null);
       mainView.router.loadPage('index.html');
     });
+  });
+
+  $$('.book-detail-container').on('click', '.book-favorite', function() {
+    $$(this).toggleClass('favorite');
+    // get state 1 = favorite, 0 = not favorite
+    let state = $$(this).hasClass('favorite') ? 1 : 0;
+
+    // open connection to db to update book
+    var db  = new DBHandler();
+    var dbh = db.getDBH();
+    dbh.transaction(function(tx) {
+      tx.executeSql('update books set favorite = ? where id = ?', [state, $$('.book-detail').data('id')], null, null);
+    });
+  });
+
+  $$('.book-detail-container').on('click', '.book-read', function() {
+    $$(this).toggleClass('read');
+    // get state 1 = read, 0 = not read
+    let state = $$(this).hasClass('read') ? 1 : 0;
+
+    // open connection to db to update book
+    var db  = new DBHandler();
+    var dbh = db.getDBH();
+    dbh.transaction(function(tx) {
+      tx.executeSql('update books set read = ? where id = ?', [state, $$('.book-detail').data('id')], null, null);
+    });
+
   });
 
 });
